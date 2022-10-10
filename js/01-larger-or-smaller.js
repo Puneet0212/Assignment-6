60467a2bc82a1ca86f68d1603a93b199ac4c56ec
Puneet0212 <puneet.patil1999@gmail.com>
2022-10-09 22:49:16 -0700

    Updated

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,9 +1,12 @@
-var firstNumber = window.prompt("Enter the first integer:");
-var secondNumber = window.prompt("Enter the second integer:");
+var firstNumber = parseInt(window.prompt("Enter the first integer:"));
+var secondNumber = parseInt(window.prompt("Enter the second integer:"));
 
-if(firstNumber >= secondNumber){
-    window.document.write("The larger number is " + firstNumber);
+if(firstNumber > secondNumber){
+    window.document.write("The first number is larger than the second: " + firstNumber);
+}
+else if(secondNumber > firstNumber){
+    window.document.write("The second number is larger than the first: " + secondNumber);
 }
 else{
-    window.document.write("The larger number is " + secondNumber);
+    window.document.write("Both the numbers are equal!");
 }
--- js/02-coinflip.js
+++ js/02-coinflip.js
@@ -4,32 +4,55 @@ var result="";
 
 window.console.log("************************Coin Flip************************");
 window.console.log(coinFlip);
+
+// // Part 1-7
+// //Check the Result
+// if(coinFlip>=47){
+//     result="Heads";
+// }
+// else{
+//     result="Tails";
+// }
+
+// if(result == "Heads" && choice == "Heads") {
+//     window.document.write("The flip was heads and you chose heads...you win!");
+// }
+
+// else if(result == "Heads" && choice == "Tails") {
+//     window.document.write("The flip was heads but you chose tails...you lose!");
+// }
+
+// else if(result == "Tails" && choice == "Heads") {
+//     window.document.write("The flip was tails but you chose heads...you lose!");
+// }
+
+// else if(result == "Tails" && choice == "Tails") {
+//     window.document.write("The flip was tails and you chose tails...you win!");
+// }
+
+//Part 8
 //Check the Result
 if(coinFlip>=47){
-    result="Heads";
+    result=true;
 }
 else{
-    result="Tails";
+    result=false;
 }
 
-if(result=="Heads"){
-    if(choice==="Heads"){
-        window.alert("The flip was heads and you chose heads...you win");
-    }
-    else if(choice==="Tails"){
-        window.alert("The flip was heads but you chose tails...you lose!");
-    }
+if(result && choice == "Heads") {
+    window.document.write("The flip was heads and you chose heads...you win!");
 }
-else if(result=="Tails"){
-    if(choice==="Heads"){
-        window.alert("The flip was tails but you chose heads...you lose!");
-    }
-    else if(choice==="Tails"){
-        window.alert("The flip was tails and you chose tails...you win!");
-    }
-    else{
-        window.alert("Invalid choice");
-    }
+
+else if(result && choice == "Tails") {
+    window.document.write("The flip was heads but you chose tails...you lose!");
+}
+
+else if(!result && choice == "Heads") {
+    window.document.write("The flip was tails but you chose heads...you lose!");
+}
+
+else if(!result && choice == "Tails") {
+    window.document.write("The flip was tails and you chose tails...you win!");
 }
 
 
